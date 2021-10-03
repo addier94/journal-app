c239cf11077ffd655b3786002526b0fcdb8f0aa8
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-10-02 22:25:00 -0400

    Thunk Middleware - Actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,14 @@
 import { types } from '../types/types'
 
+export const startLoginEmailPassword = (email, password) => {
+  return (dispatch) => {
+    
+    setTimeout(() => {
+      dispatch( login(123, 'Alfredo') );
+    }, 3500);
+  }
+}
+
 export const login = (uid, displayName) => ({
   type: types.login,
   payload: {
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 
 import { Link } from 'react-router-dom'
-import { login } from '../../actions/auth';
+import { startLoginEmailPassword } from '../../actions/auth';
 import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
@@ -18,7 +18,7 @@ export const LoginScreen = () => {
 
     const handleLogin = (e) => {
         e.preventDefault();
-        dispatch( login(12345, 'Alfredo') )
+        dispatch( startLoginEmailPassword(email, password) )
     }
 
     return (
