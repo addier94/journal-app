fea699beddeb083b24af811228b3373f51876edf
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-10-03 17:02:54 -0400

    uiReducer and action

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import validator from 'validator';
+import { useDispatch } from 'react-redux';
+import { removeError, setError } from '../../actions/ui';
 
 export const RegisterScreen = () => {
+    const dispatch = useDispatch();
     const [ formValues, handleInputChange ] = useForm({
         name: 'Alfredo',
         email: 'alfredo@gmail.com',
@@ -16,22 +19,22 @@ export const RegisterScreen = () => {
     const handleRegister = (e) => {
         e.preventDefault();
         if ( isFormValid() ) {
-            console.log('success data')
         }
     }
 
     const isFormValid = () => {
 
         if( name.trim().length === 0 ){
-            console.log('Name is required')
+            dispatch(setError('Name is required'))
             return false
         } else if (!validator.isEmail( email )){
-            console.log('email is not valid')
+            dispatch(setError('email is not valid'))
             return false
         } else if ( password !== password2 || password.length < 5) {
-            console.log('Password should be at least 6 characters and match each other')
+            dispatch(setError('Password should be at least 6 characters and match each other'))
             return false
         }
+        dispatch(removeError())
 
         return true
     }
