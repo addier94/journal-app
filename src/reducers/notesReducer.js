79d01211ceb3cec08d5d723441c4dbb1d7524dc7
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-10-04 19:18:34 -0400

    NotesReducer

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  notes: [],
+  active: null
+}
+
+export const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+  
+    default:
+      return state;
+  }
+}
