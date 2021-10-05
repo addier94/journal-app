6cfd52ccc31c70c7700041d179320fcd3fbb9ad1
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-10-04 21:32:31 -0400

    Active the create note

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,5 +1,6 @@
 import { db } from "../firebase/firebase-config";
 import { collection, addDoc } from "@firebase/firestore";
+import { types } from "../types/types";
 
 
 
@@ -18,6 +19,15 @@ export const startNewNote = () => {
 
     const doc = await addDoc(collection(db, `${ uid }`, '/journal/notes'), newNote);
 
-    console.log(doc)
+    dispatch( activeNote( doc.id, newNote ));
+
   }
 }
+
+export const activeNote = ( id, note ) => ({
+  type: types.notesActive,
+  payload: {
+    id,
+    ...note
+  }
+})
